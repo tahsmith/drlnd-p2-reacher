74f76e1a0d0911aa26d4e9b6cc50408430475ef6
tahsmith <tahs88@gmail.com>
2018-12-18 20:28:13 +1100

    Revert back to own models

--- actor.py
+++ actor.py
@@ -1,82 +1,37 @@
-# import torch
-# from torch import nn
-# from torch.nn import functional as F
-# import numpy as np
-#
-#
-# class Actor(nn.Module):
-#     def __init__(self, state_size, action_size, action_range=None):
-#         super(Actor, self).__init__()
-#         hidden_units = 256
-#
-#         if action_range is None:
-#             action_range = [[-1, 1] for _ in range(action_size)]
-#         action_range = np.array(action_range)
-#         self.action_0 = torch.from_numpy(action_range[:, 0]).float()
-#         self.action_range = torch.from_numpy(
-#             np.diff(action_range, axis=1)[:, 0]).float()
-#
-#         self.dropout = nn.Dropout()
-#         self.pi = nn.Sequential(
-#             nn.Linear(in_features=state_size, out_features=hidden_units),
-#             nn.ELU(),
-#             self.dropout,
-#             nn.Linear(in_features=hidden_units, out_features=hidden_units),
-#             nn.ELU(),
-#             nn.Linear(in_features=hidden_units, out_features=action_size),
-#         )
-#
-#     def forward(self, state):
-#         x = self.pi(state)
-#         x = F.tanh(x)
-#         return x
-#
-#     def to(self, *args, **kwargs):
-#         self.action_0 = self.action_0.to(*args, **kwargs)
-#         self.action_range = self.action_range.to(*args, **kwargs)
-#         return super(Actor, self).to(*args, **kwargs)
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn
+from torch.nn import functional as F
 import numpy as np
 
 
-def hidden_init(layer):
-    fan_in = layer.weight.data.size()[0]
-    lim = 1. / np.sqrt(fan_in)
-    return (-lim, lim)
-
-
 class Actor(nn.Module):
-    """Actor (Policy) Model."""
-
-    def __init__(self, state_size, action_size, action_range=None, seed=32,
-                 fc1_units=256,
-                 fc2_units=256):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-        """
+    def __init__(self, state_size, action_size, action_range=None):
         super(Actor, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, action_size)
-        self.b2 = nn.BatchNorm1d(action_size)
-        self.tanh = nn.Tanh()
-        self.reset_parameters()
+        hidden_units = 256
 
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
+        if action_range is None:
+            action_range = [[-1, 1] for _ in range(action_size)]
+        action_range = np.array(action_range)
+        self.action_0 = torch.from_numpy(action_range[:, 0]).float()
+        self.action_range = torch.from_numpy(
+            np.diff(action_range, axis=1)[:, 0]).float()
+
+        self.dropout = nn.Dropout()
+        self.pi = nn.Sequential(
+            nn.Linear(in_features=state_size, out_features=400),
+            nn.ELU(),
+            self.dropout,
+            nn.Linear(in_features=400, out_features=300),
+            nn.ELU(),
+            nn.Linear(in_features=300, out_features=action_size),
+        )
 
     def forward(self, state):
-        x = F.leaky_relu(self.fc1(state))
-        x = F.leaky_relu(self.fc2(x))
-        x = self.b2(x)
-        x = self.tanh(x)
-
+        x = self.pi(state)
+        x = F.tanh(x)
         return x
+
+    def to(self, *args, **kwargs):
+        self.action_0 = self.action_0.to(*args, **kwargs)
+        self.action_range = self.action_range.to(*args, **kwargs)
+        return super(Actor, self).to(*args, **kwargs)
--- critic.py
+++ critic.py
@@ -3,70 +3,27 @@ from torch import nn
 from torch.nn import functional as F
 import numpy as np
 
-#
-# class Critic(nn.Module):
-#     def __init__(self, state_size, action_size):
-#         super(Critic, self).__init__()
-#         hidden_units = 128
-#         self.dropout = nn.Dropout()
-#
-#         self.state_layer = nn.Sequential(
-#             nn.Linear(in_features=state_size, out_features=hidden_units),
-#             nn.ELU()
-#         )
-#         self.qa = nn.Sequential(
-#             nn.ELU(),
-#             self.dropout,
-#             nn.Linear(in_features=hidden_units + action_size,
-#                       out_features=hidden_units),
-#             nn.ELU(),
-#             nn.Linear(in_features=hidden_units, out_features=hidden_units),
-#             nn.ELU(),
-#             nn.Linear(in_features=hidden_units, out_features=1),
-#         )
-#
-#     def forward(self, state, action):
-#         x = self.state_layer(state)
-#         x = torch.cat([x, action], dim=1)
-#         return self.qa(x)
-
-
-def hidden_init(layer):
-    fan_in = layer.weight.data.size()[0]
-    lim = 1. / np.sqrt(fan_in)
-    return (-lim, lim)
-
 
 class Critic(nn.Module):
-    """Critic (Value) Model."""
-
-    def __init__(self, state_size, action_size, seed=32, fc1_units=128,
-                 fc2_units=128, fc3_units=128, fc4_units=128):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimesion of each action
-            seed (int): Random seed
-        """
+    def __init__(self, state_size, action_size):
         super(Critic, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc2_units + action_size, fc2_units)
-        self.fc3 = nn.Linear(fc3_units, fc4_units)
-        self.fc4 = nn.Linear(fc4_units, 1)
+        hidden_units = 128
+        self.dropout = nn.Dropout()
 
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
+        self.state_layer = nn.Sequential(
+            nn.Linear(in_features=state_size, out_features=400),
+            nn.ELU()
+        )
+        self.qa = nn.Sequential(
+            nn.ELU(),
+            self.dropout,
+            nn.Linear(in_features=400 + action_size,
+                      out_features=300),
+            nn.ELU(),
+            nn.Linear(in_features=300, out_features=1),
+        )
 
     def forward(self, state, action):
-        xs = F.leaky_relu(self.fc1(state))
-        x = torch.cat((xs, action), dim=1)
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
-        x = self.fc4(x)
-
-        return x
+        x = self.state_layer(state)
+        x = torch.cat([x, action], dim=1)
+        return self.qa(x)
